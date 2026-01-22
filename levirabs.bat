8461f5d0c019c3be9da454375c283d7b93d37f0f
ZhangAilan <ailanzhang1109@gmail.com>
2026-01-22 17:04:45 +0800

    fix name

--- levirabs.bat
+++ levirabs.bat
@@ -43,12 +43,12 @@ if %START_STEP% LEQ 3 (
 
 if %START_STEP% LEQ 4 (
     echo [Step 4] Train change detection model
-    python train_change_detection.py --data_dir %DATA_DIR% --tag LEVIR_weakly_change_detection --label_name LEVIR_KD_T_abs_S_cat@train@scale=0.5,1.0,1.25,2.0@crf=0@255 --fully_supervised False
+    python train_change_detection.py --data_dir %DATA_DIR% --tag LEVIR_abs_weakly_change_detection --label_name LEVIR_KD_T_abs_S_cat@train@scale=0.5,1.0,1.25,2.0@crf=0@255 --fully_supervised False
 )
 
 if %START_STEP% LEQ 5 (
     echo [Step 5] Inference change detection
-    python inference_change_detection.py --data_dir %DATA_DIR% --tag LEVIR_weakly_change_detection --scales 0.5,1.0,1.5,2.0
+    python inference_change_detection.py --data_dir %DATA_DIR% --tag LEVIR_abs_weakly_change_detection --scales 0.5,1.0,1.5,2.0
 )
 
 echo.
